28c52faa2376fb32ee43e6e1e5502e1bc5b6706a
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-10-14 22:56:06 -0400

    useReducer implemented for input state management

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,61 @@
-import { useState } from 'react';
+import {  useReducer } from 'react';
 import classes from './Form.module.css'
+const reducer =(state, action)=>{
+  switch(action.type){
+    case "HANDLE INPUT TEXT":
+      return {
+        ...state,
+        [action.field]: action.payload,
+      }
+      default: 
+        return state
+  }
+}
 
 const Form =(props)=>{
+  const initialState = {
+    full_name: "",
+    profession: "",
+    image: "",
+    skills: [{skill: "" }],
+    projects: [{
+      title: "",
+      image: "",
+      description: "",
+      link: ""
+    }]
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   
+  const handleInputChange = (e)=>{
+    dispatch({
+      type: "HANDLE INPUT TEXT",
+      field: e.target.name,
+      payload: e.target.value,
+    })
+    console.log(e.target.name)
+    console.log(e.target.value)
+  }
+  console.log(state.full_name)
   return (
     <form >
       <div className={classes.card}>
-      <label>First Name </label>
-      <input type="text" />
+
+      <label>Full Name </label>
+
+      <input value={state.full_name} onChange={ handleInputChange } type="text" name='full_name'/>
+
+
       </div>
-      <div className={classes.card}>
-      <label>Last Name </label>
-      <input type="text" />
-      </div>
-      <div className={classes.card}>
+      {/* <div className={classes.card}>
       <label>Profession </label>
-      <input type="text" />
+      <input onChange={ handleInputChange } type="text" name='profession'/>
       </div>
-      <br />
+      <div className={classes.card}>
+      <label>Image </label>
+      <input  type="text" value={state.image} name='image'/>
+      </div>
+      <br /> */}
       <button>Submit</button>
     </form>
   )
